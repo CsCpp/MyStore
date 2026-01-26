dee34167d4188ca8e952b0d43d6b6814c692da37
CsCppKotlin <CsCppKotlin@gmail.com>
2026-01-26 11:42:40 +0300

    ShopComputerModel

---
+++ UnitTestProject1/ShopComputerModelTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using  CrmBl.Model;
+using System.Collections.Generic;
+
+namespace CrmBl.Model.Tests
+{
+    [TestClass]
+    public class ShopComputerModelTest
+    {
+        [TestMethod]
+        public void StartTest()
+        {
+            //arrange
+          var model = new ShopComputerModel();
+
+
+            //act
+            model.Start();
+
+            //assert
+          
+
+
+        }
+    }
+}
